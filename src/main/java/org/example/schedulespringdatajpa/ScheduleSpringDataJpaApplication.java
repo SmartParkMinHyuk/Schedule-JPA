00145d2c12454dc9df48b2c3c2d4081d6ff10313
DESKTOP-M7S61C8\박수박 <pospara2@gmail.com>
2025-04-06 18:02:07 +0900

    chor : project sttings

---
+++ src/main/java/org/example/schedulespringdatajpa/ScheduleSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedulespringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/schedulespringdatajpa/ScheduleSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedulespringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
